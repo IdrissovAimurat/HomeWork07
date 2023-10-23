d8c909c2311809f10f6e346ae1c27be9eb53bf98
call me ai|sem <aimur349Gmail.com>
2023-10-24 01:20:25 +0600

    Soryan ne tot commit byl v assignment

---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        private string name;
+        private int ammoLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+
+        public Tank(string name, int ammoLevel, int armorLevel, int maneuverabilityLevel)
+        {
+            this.name = name;
+            this.ammoLevel = ammoLevel;
+            this.armorLevel = armorLevel;
+            this.maneuverabilityLevel = maneuverabilityLevel;
+        }
+
+        public static bool operator ^(Tank tank1, Tank tank2)
+        {
+            int winningCriteriaCount = 0;
+            if (tank1.ammoLevel > tank2.ammoLevel) winningCriteriaCount++;
+            if (tank1.armorLevel > tank2.armorLevel) winningCriteriaCount++;
+            if (tank1.maneuverabilityLevel > tank2.maneuverabilityLevel) winningCriteriaCount++;
+
+            return winningCriteriaCount >= 2;
+        }
+
+        public override string ToString()
+        {
+            return $"Tank: {name}, Уровень боезапаса: {ammoLevel}, Уровень брони: {armorLevel}, Уровень маневренности: {maneuverabilityLevel}";
+        }
+        public static bool operator ==(Tank tank1, Tank tank2)
+        {
+            return tank1.Equals(tank2);
+        }
+
+        public static bool operator !=(Tank tank1, Tank tank2)
+        {
+            return !tank1.Equals(tank2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Tank tank = (Tank)obj;
+            return name == tank.name;
+        }
+    }
+}
---
+++ TigerTank/Program.cs
@@ -0,0 +1,44 @@
+﻿using MyClassLib.WordOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TigerTank
+{
+    public class Program
+    {
+        /// <summary>
+        /// https://www.youtube.com/watch?v=c6lgJoH5NMc&ab_channel=JustGamer
+        /// 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Tank[] T34Tanks = new Tank[5];
+
+            Tank[] PanteraTanks = new Tank[5];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                T34Tanks[i] = new Tank("\nT-34 " + (i + 1), rand.Next(0, 101), rand.Next(0, 101), rand.Next(0, 101));
+
+                PanteraTanks[i] = new Tank("\nPantera " + (i + 1), rand.Next(0, 101), rand.Next(0, 101), rand.Next(0, 101));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"\n{T34Tanks[i]} vs {PanteraTanks[i]}");
+
+                bool T34Wins = T34Tanks[i] ^ PanteraTanks[i];
+
+                Console.WriteLine(T34Wins ? "\nИтог: \tT-34 Пoбедил, ИУУУУУУ. Есть пробитие!" : 
+                                            "\nИтог: \tPantera Победил, Мой Ф***р");
+            }
+            Console.ReadKey();
+        }
+    }
+}
